51faf11a50ee95afd9ff592dfb9aca9e01f82c12
KJ1234-dev <kushagrajoshi69@gmail.com>
2026-04-15 10:57:46 +0530

    Update Common.h

--- include/Common.h
+++ include/Common.h
@@ -6,19 +6,20 @@
 #include <unordered_map>
 #include <cstdint>
 
-// --- SHARED TOKENS ---
-enum TokenType { 
+// --- TOKENS ---
+enum TokenType {
     TOK_LET, TOK_PRINT,
+    TOK_IF, TOK_ELSE,
+
     TOK_IDENT, TOK_INT,
 
-    // Operators
     TOK_PLUS, TOK_MINUS, TOK_STAR, TOK_SLASH,
-
-    // Symbols
     TOK_ASSIGN, TOK_SEMI,
-    TOK_LPAREN, TOK_RPAREN,
 
-    TOK_EOF 
+    TOK_LPAREN, TOK_RPAREN,
+    TOK_LBRACE, TOK_RBRACE,
+
+    TOK_EOF
 };
 
 struct Token {
@@ -26,18 +27,16 @@ struct Token {
     std::string lexeme;
 };
 
-// --- SHARED OPCODES ---
+// --- OPCODES ---
 enum Opcode : uint8_t {
-    OP_PUSH,    // Push integer onto stack
+    OP_PUSH,
+    OP_ADD, OP_SUB, OP_MUL, OP_DIV,
 
-    OP_ADD,     // a + b
-    OP_SUB,     // a - b
-    OP_MUL,     // a * b
-    OP_DIV,     // a / b
+    OP_STORE, OP_LOAD,
+    OP_PRINT,
 
-    OP_STORE,   // store variable
-    OP_LOAD,    // load variable
+    OP_JUMP,
+    OP_JUMP_IF_FALSE,
 
-    OP_PRINT,   // print value
-    OP_HALT     // stop execution
+    OP_HALT
 };
